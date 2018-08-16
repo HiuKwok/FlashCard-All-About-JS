7a4aa64f80c4dae45162bfdc5789134c2b1f80d6
Hiu Kwok <hiufkwok@gmail.com>
2018-08-16 18:36:42 +0800

    Example added: Date with moment.js

--- Basic/DateTime.js
+++ Basic/DateTime.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 var date = new Date();
 
 
@@ -5,3 +7,11 @@ console.log(date)
 
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 console.log(date.toLocaleDateString('ko-KR', options));
+
+
+//It would auto handle the timezone as it unixTime is tz-insensitive
+const m = moment("2018-08-16 17:55:53", "YYYY-MM-DD HH:mm:ss");
+const unixTime = m.unix();
+//Re Parse
+const after = moment( unixTime, 'X' );
+console.log(after);
